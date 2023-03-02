e0f3b8a3304925b023b36606a4724cf05d8bfe24
JongHak Seo <unqocn@gmail.com>
2023-03-02 09:41:25 +0900

    chatGPT api

--- chatGPT.ts
+++ chatGPT.ts
@@ -13,21 +13,24 @@ type ChatGPTInput = {
 }
 
 export async function chatGPT({ prompt }: ChatGPTInput): Promise<string> {
-  const completion = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt,
+  const completion =await openai.createChatCompletion({
+    model:"gpt-3.5-turbo",
+    // max 4000
+    max_tokens: 4000,
+    messages:[
+      {"role": "system", "content": "You are ChatGPT, a large language model trained by OpenAI. Answer as concisely as possible."},
+      {"role": "user", "content": prompt },
+    ],
     // 의외성 (0~1)
     temperature: 0.7,
-    // max 4000
-    max_tokens: 3000,
     // 단어 풀의 범위(0~1)
     top_p: 1,
     // 자주 사용하는 단어 억제
     frequency_penalty: 0.2,
     // 이미 사용된 단어 억제
     presence_penalty: 0.1
-  });
+  })
 
-  return completion.data.choices.at(0)?.text ?? "알 수 없는 에러"
+  return completion.data.choices.at(0)?.message.content ?? "알 수 없는 에러"
 }
 
