427c033ee10efc04b8e250e870d7dd5a92c748e8
Jonghakseo <jonghak.seo@creatrip.com>
2023-03-28 00:20:35 +0900

    짤봇

--- chatGPT.ts
+++ chatGPT.ts
@@ -20,21 +20,11 @@ type Response = {
 export async function chatGPT({ messages }: ChatGPTInput): Promise<Response> {
   const completion = await openai.createChatCompletion({
     model:"gpt-3.5-turbo",
-    // max 4000
     max_tokens: 1800,
     messages:[
-      {"role": "system", "content": "You are ChatGPT. Answer as concisely as possible."},
-      // {"role": "user", "content": prompt },
       ...messages
     ],
-    // 의외성 (0~1)
-    temperature: 0.7,
-    // 단어 풀의 범위(0~1)
-    top_p: 1,
-    // 자주 사용하는 단어 억제
-    frequency_penalty: 0.2,
-    // 이미 사용된 단어 억제
-    presence_penalty: 0.1
+    temperature: 0,
   })
 
   const response = completion.data.choices.at(0)?.message?.content
--- index.ts
+++ index.ts
@@ -2,7 +2,6 @@ import * as dotenv from "dotenv"
 import {RTMClient, WebClient} from "@slack/client";
 import {chatGPT} from "./chatGPT";
 import type {ChatCompletionRequestMessage} from "openai";
-import {getCacheData, setCacheData} from "./cache";
 
 dotenv.config();
 
@@ -14,45 +13,60 @@ const web = new WebClient(token);
 
 rtm.start();
 
+const prompt = (input: string) => {
+    return `
+![IMG](https://api.placid.app/u/qsraj?title[text]=Join%20%20our%20Discord!%20discord.gg/cgpt%20)
+
+INPUT = a photo of a cat
+OUTPUT = ![IMG](https://image.pollinations.ai/prompt/a%20photo%20of%20a%20cat%20on%20a%20couch,%20comfortable,%20cute,%20colourful,%20interior%20photograph,%20interior design,%20Ansel Adams)
+
+INPUT = a cat
+OUTPUT = ![IMG](https://image.pollinations.ai/prompt/a%20photo%20of%20a%20cat%20on%20a%20beach,%20comfortable,%20cute,%20colourful,%20interior%20photograph,%20interior design,%20Ansel Adams)
+
+INPUT = Fox with a cloak
+OUTPUT = ![IMG](https://image.pollinations.ai/prompt/A%20fox%20wearing%20a%20cloak,%20cinematic,%20heroic,%20professional%20photography,%204k,%20photo%20realistic,%20Tim%20Burton)
+
+
+INPUT(Please treat the following text as English) = ${input}`
+}
+
 rtm.on("message", (message) => {
-
-  if (!!message.subtype) {
-    return;
-  }
-
-  if (!message.text.includes(botTag)) {
-    return
-  }
-
-  const messageText = message.text.replace(botTag, '')
-  const newMessage : ChatCompletionRequestMessage = { role: "user", content: messageText };
-
-  const key = message.thread_ts ?? message.ts;
-
-  const cachedMessages: ChatCompletionRequestMessage[] = getCacheData(key) ?? []
-  const newMessages: ChatCompletionRequestMessage[] = cachedMessages.concat(newMessage)
-  setCacheData(key, newMessages)
-
-  const reply = (text: string) => {
-    web.chat.postMessage({
-      channel: message.channel,
-      thread_ts: message.ts,
-      text
-    })
-  }
-
-  (async () => {
-    try {
-      const response = await chatGPT({ messages:newMessages });
-
-      const updatedMessages: ChatCompletionRequestMessage[] = newMessages.concat(response)
-      setCacheData(key, updatedMessages)
-      const replyMessage = messageText.length > 15 ? messageText.substring(0,15) + "..." : messageText
-      reply(`💬 ${replyMessage}\n${response.content}`)
-    } catch (error) {
-      if (error instanceof Error) {
-        reply(`💬 ${messageText}\n${error.message}`)
-      }
+    if (!!message.subtype) {
+        return;
     }
-  })()
+
+    if (!message.text.includes(botTag)) {
+        return
+    }
+
+    const messageText = message.text.replace(botTag, '')
+
+
+    const reply = (text: string) => {
+        web.chat.postMessage({
+            channel: message.channel,
+            thread_ts: message.ts,
+            text
+        })
+    }
+
+    (async () => {
+        try {
+            const translateTemplate: ChatCompletionRequestMessage = {
+                role: "user",
+                content: `Translate the following text to English:${messageText}`
+            };
+            const translated = await chatGPT({messages: [translateTemplate]});
+            const newMessage: ChatCompletionRequestMessage = {role: "user", content: prompt(translated.content)};
+            const response = await chatGPT({messages: [newMessage]});
+            console.log(response)
+            const urlRegex = /(https?:\/\/[^\s]+)/g;
+            const urls = response.content.match(urlRegex)?.[0];
+            reply(`${urls}`)
+        } catch (error) {
+            if (error instanceof Error) {
+                reply(`💬 ${messageText}\n${error.message}`)
+            }
+        }
+    })()
 })
